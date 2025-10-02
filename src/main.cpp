c487a50c46c238cd698dd1e2835e81d4deb86d1a
WanekNia <nathan.wanecque@outlook.com>
2025-10-03 00:53:41 +0200

    Removed log file Executing in terminal is better

--- src/main.cpp
+++ src/main.cpp
@@ -10,39 +10,6 @@
 
 QFile logFile;
 
-void myMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
-    const QString logMessage = QString("[%1] %2")
-        .arg(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss"), msg);
-
-    // 1. Écrit dans le fichier
-    if (logFile.isOpen()) {
-        QTextStream ts(&logFile);
-        ts << logMessage << Qt::endl;
-    }
-
-    // 2. Continue aussi d'afficher dans la console CLion
-    fprintf(stderr, "%s\n", logMessage.toLocal8Bit().constData());
-    fflush(stderr);
-}
-
-void installDesktopFile()
-{
-    qInfo() << "installDesktopFile()";
-
-    const QString appsDir = QStandardPaths::writableLocation(QStandardPaths::ApplicationsLocation);
-    const QString desktopDestFile = appsDir + "/eve-w-preview.desktop";
-    if (QFile::exists(desktopDestFile)) {
-        return;
-    }
-    QFile src(":/scripts/eve-w-preview.desktop");
-    if (!src.exists()) {
-        return;
-    }
-    if (!src.copy(desktopDestFile)) {
-        qWarning() << "[installDesktopFile] Failed to copy to:" << desktopDestFile;
-    }
-}
-
 int main(int argc, char *argv[])
 {
     // Logs in file
@@ -51,7 +18,6 @@ int main(int argc, char *argv[])
         QFile::remove(logFile.fileName());
     }
     logFile.open(QIODevice::WriteOnly | QIODevice::Text);
-    qInstallMessageHandler(myMessageHandler);
 
     // Options de lancement
     qputenv("QT_QPA_PLATFORM", "xcb");
@@ -60,7 +26,7 @@ int main(int argc, char *argv[])
     qputenv("QT_XCB_NO_XI2", "1");
 
     QApplication app(argc, argv);
-    app.setWindowIcon(QIcon(":/assets/EveWPreview.png"));
+    QApplication::setWindowIcon(QIcon(":/assets/EveWPreview.png"));
 
     EveWPreviewWindow* eveWPreviewWindow = new EveWPreviewWindow();
     eveWPreviewWindow->show(); // Affiche MainWindow
